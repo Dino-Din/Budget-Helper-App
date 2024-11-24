89b5d52defa675c2f84ae92b86d9437ef95401fb
Dinithi Abeywardena <hewa.abeywardena@mail.utoronto.ca>
2024-11-24 14:08:37 -0500

    add backend

---
+++ backend/budget-helper/src/test/java/dna/budget/helper/project/budgethelper/BudgetHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package dna.budget.helper.project.budgethelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
